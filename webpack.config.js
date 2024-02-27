904f0f05a16857ff0d3e98ae9d5779cdcd0b18d7
Alexandra Luka <lenimide@gmail.com>
2024-02-27 14:47:47 +0500

    corrected

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,24 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
-  
+  module: {
+    rules: [
+      {
+        test: /\.css$/, 
+        use: [
+          MiniCSSExtractPlugin.loader, 'css-loader'
+        ],
+      },
+    ]
+  },
+
+
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html"
+    }),
+    new MiniCSSExtractPlugin()
+  ],
 };
